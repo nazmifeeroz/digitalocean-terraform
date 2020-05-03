3c85d7b1a86f959358e9d7a4f8f0ab4684f3430d
Nazmi <nazmifeeroz@hotmail.com>
2020-05-03 08:22:51 +0800

    exporting jwt_secret requires quotes. works!

--- do.tf
+++ do.tf
@@ -61,14 +61,14 @@ resource "digitalocean_droplet" "web" {
             "mkdir hasura && cd hasura",
             "wget https://raw.githubusercontent.com/nazmifeeroz/digitalocean-terraform/master/docker-compose.yaml",
             "export ADMIN_SECRET=${var.hasura_admin_secret}",
-            "export JWT_SECRET=${var.hasura_jwt_secret}",
+            "export JWT_SECRET='${var.hasura_jwt_secret}'",
+            "echo $ADMIN_SECRET",
+            "echo $JWT_SECRET",
             "printf '\n\n===================\n'",
             "sudo -E docker-compose config",
             "printf '\n===================\n\n'",
             "sudo -E docker-compose up -d",
-            "printf '\n\n===================\nHasura installed successfully!\n===================\n\n'",
-            "echo $ADMIN_SECRET",
-            "echo $JWT_SECRET"
+            "printf '\n\n===================\nHasura installed successfully!\n===================\n\n'"
         ]
     }
 }
